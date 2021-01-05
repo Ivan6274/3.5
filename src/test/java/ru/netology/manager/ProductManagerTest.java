87817c847330e72453cc8d109f26f08ed8904d8e
Ivan6274 <isivanov2011@gmail.com>
2021-01-05 17:28:49 +0300

    Fix commit#2

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -40,9 +40,9 @@ class ProductManagerTest {
         Book firstBook = new Book(4, "firstBook", 100, "i'm");
         Smartphone samsungB2 = new Smartphone(2, "Samsung B2", 1000, "samsung");
         Book secondBook = new Book(3, "secondBook", 150, "i'm");
-        Smartphone apple = new Smartphone(2, "Apple", 1000, "apple");
+        Smartphone apple = new Smartphone(2, "AppleXR", 1000, "apple");
         Smartphone samsungA1 = new Smartphone(2, "Samsung A1", 1000, "samsung");
-        Smartphone samsungT10 = new Smartphone(2, "Samsung A1", 1000, "samsung");
+        Smartphone samsungT10 = new Smartphone(2, "Samsung T10", 1000, "samsung");
 
         productManager.add(javaCore);
         productManager.add(firstBook);
@@ -58,9 +58,24 @@ class ProductManagerTest {
         assertArrayEquals(actual,expected);
 
         Product[] actual1 = productManager.searchBy("i'm");
-        Product[] expected2 = new Product[]{firstBook, secondBook};
+        Product[] expected1 = new Product[]{firstBook, secondBook};
 
-        assertArrayEquals(actual1,expected2);
+        assertArrayEquals(actual1,expected1);
+
+        Product[] actual2 = productManager.searchBy("nokia");
+        Product[] expected2 = new Product[]{};
+
+        assertArrayEquals(actual2,expected2);
+
+        Product[] actual3 = productManager.searchBy("AppleXR");
+        Product[] expected3 = new Product[]{apple};
+
+        assertArrayEquals(actual3,expected3);
+
+        Product[] actual4 = productManager.searchBy("javaCore");
+        Product[] expected4 = new Product[]{javaCore};
+
+        assertArrayEquals(actual4,expected4);
 
     }
 
